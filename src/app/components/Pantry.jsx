779d62e7a3f570f05c070203c43e9af885ec1177
Tabindanoor <tabindanoor415@gmail.com>
2024-08-18 23:01:33 +0500

    workinh

--- src/app/components/Pantry.jsx
+++ src/app/components/Pantry.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const Pantry = () => {
   const [todo, setTodo] = useState([])
+  const [editTodo,setEditTodo]  = useState([])
   return (
     <div>Pantry</div>
   )
