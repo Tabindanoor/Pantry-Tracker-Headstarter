0dd77d541571668413ae23d66f744e1935b71078
Tabindanoor <tabindanoor415@gmail.com>
2024-08-19 22:57:25 +0500

    workinh

--- src/app/components/Pantry.jsx
+++ src/app/components/Pantry.jsx
@@ -1,11 +1,1 @@
-import React, { useState } from 'react'
 
-const Pantry = () => {
-  const [todo, setTodo] = useState([])
-  const [editTodo,setEditTodo]  = useState([])
-  return (
-    <div>Pantry</div>
-  )
-}
-
-export default Pantry
