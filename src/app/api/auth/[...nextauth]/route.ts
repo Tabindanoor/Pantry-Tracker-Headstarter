68a20a52ffb4ef735c6b4da2fda066dc927a362c
Tabindanoor <tabindanoor415@gmail.com>
2024-09-02 23:52:56 +0500

    using the google provider

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,52 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaClient } from '@prisma/client';
+import {compare} from "bcryptjs"
+import GoogleProvider from "next-auth/providers/google"
+const prisma = new PrismaClient();
+
+const handler = NextAuth({
+  providers: [
+    // CredentialsProvider({
+    //   name: 'Credentials',
+    //   credentials: {
+    //     email: { label: "Email", type: "email" },
+    //     password: { label: "Password", type: "password" }
+    //   },
+    //   async authorize(credentials) {
+    //     const user = await prisma.user.findUnique({
+    //       where: { email: credentials?.email }
+    //     });
+
+    //     if (user && (await compare(credentials?.password, user.password))) {
+    //       return { id: user.id, email: user.email, name: user.name, image: user.image };
+    //     }
+
+    //     return null;
+    //   }
+    // }),
+
+    GoogleProvider({
+            clientId: process.env.GOOGLE_CLOUD_ID as string,
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET as string,
+
+
+    })
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      const user = await prisma.user.findUnique({ where: { id: token.sub } });
+      session.user = user;
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) token.sub = user.id;
+      return token;
+    }
+  },
+  pages: {
+    signIn: '/auth/signin',
+  }
+});
+
+export { handler as GET, handler as POST };
