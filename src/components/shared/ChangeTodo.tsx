7de37933051f99e0a24ec33bf9b5079038f23c53
Tabindanoor <tabindanoor415@gmail.com>
2025-05-23 16:40:33 +0500

    working

--- src/components/shared/ChangeTodo.tsx
+++ src/components/shared/ChangeTodo.tsx
@@ -1,82 +1,24 @@
-// import Form from '../ui/Form';
-// import Input from '../ui/Input';
-// import Button from '../ui/Button';
-// import { change } from "@/app/actions/TodoActions";
-// import { todoTypes } from '../../../types/todoTypes';
-
-// const ChangeTodo = ({ todo }: { todo: todoTypes }) => {
-//   return (
-//     <div>
-//       <Form action={change}>
-//         {/* Hidden input to send the todo ID */}
-//         <Input 
-//           name="inputId"
-//           type="hidden"
-//           value={todo.id}
-//         />
-//         {/* Button with SVG icon and text */}
-//         <Button
-//           actionButton
-//           type="submit"
-//           text={(
-//             <>
-//               <svg
-//                 xmlns="http://www.w3.org/2000/svg"
-//                 fill="none"
-//                 viewBox="0 0 24 24"
-//                 strokeWidth={1.5}
-//                 stroke="currentColor"
-//                 className="w-6 h-6 inline-block  bg-purple-800 rounded-full"
-//               >
-//                 <path
-//                   strokeLinecap="round"
-//                   strokeLinejoin="round"
-//                   d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
-//                 />
-//               </svg>
-              
-//             </>
-//           )}
-//         />
-//       </Form>
-//     </div>
-//   );
-// };
-
-// export default ChangeTodo;
-
-
-
-"use client"
-import React from "react"
-import Form from "../ui/Form"
-import Input from "../ui/Input"
-import Button from "../ui/Button"
-import { change } from "@/app/actions/TodoActions"
-import { todoTypes } from "../../../types/todoTypes"
-import { useRouter } from "next/navigation"
-import { useTransition } from "react"
+import Form from '../ui/Form';
+import Input from '../ui/Input';
+import Button from '../ui/Button';
+import { change } from "@/app/actions/TodoActions";
+import { todoTypes } from '../../../types/todoTypes';
 
 const ChangeTodo = ({ todo }: { todo: todoTypes }) => {
-  const router = useRouter()
-  const [isPending, startTransition] = useTransition()
-
-  // Wrap the change action to refresh UI after toggling
-  const handleChange = async (formData: FormData) => {
-    await change(formData)
-    startTransition(() => {
-      router.refresh()
-    })
-  }
-
   return (
     <div>
-      <Form action={handleChange}>
-        <Input name="inputId" type="hidden" value={todo.id} />
+      <Form action={change}>
+        {/* Hidden input to send the todo ID */}
+        <Input 
+          name="inputId"
+          type="hidden"
+          value={todo.id}
+        />
+        {/* Button with SVG icon and text */}
         <Button
           actionButton
           type="submit"
-          text={
+          text={(
             <>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -84,7 +26,7 @@ const ChangeTodo = ({ todo }: { todo: todoTypes }) => {
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="w-6 h-6 inline-block bg-purple-800 rounded-full"
+                className="w-6 h-6 inline-block  bg-purple-800 rounded-full"
               >
                 <path
                   strokeLinecap="round"
@@ -92,12 +34,70 @@ const ChangeTodo = ({ todo }: { todo: todoTypes }) => {
                   d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
                 />
               </svg>
+              
             </>
-          }
+          )}
         />
       </Form>
     </div>
-  )
-}
+  );
+};
 
-export default ChangeTodo
+export default ChangeTodo;
+
+
+
+// "use client"
+// import React from "react"
+// import Form from "../ui/Form"
+// import Input from "../ui/Input"
+// import Button from "../ui/Button"
+// import { change } from "@/app/actions/TodoActions"
+// import { todoTypes } from "../../../types/todoTypes"
+// import { useRouter } from "next/navigation"
+// import { useTransition } from "react"
+
+// const ChangeTodo = ({ todo }: { todo: todoTypes }) => {
+//   const router = useRouter()
+//   const [isPending, startTransition] = useTransition()
+
+//   // Wrap the change action to refresh UI after toggling
+//   const handleChange = async (formData: FormData) => {
+//     await change(formData)
+//     startTransition(() => {
+//       router.refresh()
+//     })
+//   }
+
+//   return (
+//     <div>
+//       <Form action={handleChange}>
+//         <Input name="inputId" type="hidden" value={todo.id} />
+//         <Button
+//           actionButton
+//           type="submit"
+//           text={
+//             <>
+//               <svg
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 fill="none"
+//                 viewBox="0 0 24 24"
+//                 strokeWidth={1.5}
+//                 stroke="currentColor"
+//                 className="w-6 h-6 inline-block bg-purple-800 rounded-full"
+//               >
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+//                 />
+//               </svg>
+//             </>
+//           }
+//         />
+//       </Form>
+//     </div>
+//   )
+// }
+
+// export default ChangeTodo
--- src/components/shared/DeleteTodo.tsx
+++ src/components/shared/DeleteTodo.tsx
@@ -1,114 +1,114 @@
 
-// "use client"
-// import React from 'react'
-// import { todoTypes } from '../../../types/todoTypes'
-// import Form from '../ui/Form'
-// import Button from '../ui/Button'
-// import { deleteTodo } from '@/app/actions/TodoActions'
-// import Input from '../ui/Input'
-
-// const DeleteTodo = ({todo}:{todo:todoTypes}) => {
-//   return (
-//     <div>
-        
-//         {/* Delete Todo {todo.id} */}
-//         <div>
-//             <Form action={deleteTodo}>
-            
-//             <Input
-//                 type='hidden'
-//                 value={todo.id}
-//                 name="inputId"
-//             />
-
-
-// <Button
-//   actionButton
-//   type="submit"
-//   text={
-//     <>
-//       <svg
-//         xmlns="http://www.w3.org/2000/svg"
-//         fill="none"
-//         viewBox="0 0 24 24"
-//         strokeWidth={1.5}
-//         stroke="currentColor"
-//         className="w-6 h-6 bg-red-800 rounded-full"
-//       >
-//         <path
-//           strokeLinecap="round"
-//           strokeLinejoin="round"
-//           d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-//         />
-//       </svg>
-//     </>
-//   }
-// />
-
-
-//             </Form>
-//         </div>
-
-
-//     </div>
-//   )
-// }
-
-// export default DeleteTodo
-
-
-
 "use client"
-import React from "react"
-import { todoTypes } from "../../../types/todoTypes"
-import Form from "../ui/Form"
-import Button from "../ui/Button"
-import { deleteTodo } from "@/app/actions/TodoActions"
-import Input from "../ui/Input"
-import { useRouter } from "next/navigation"
-import { useTransition } from "react"
+import React from 'react'
+import { todoTypes } from '../../../types/todoTypes'
+import Form from '../ui/Form'
+import Button from '../ui/Button'
+import { deleteTodo } from '@/app/actions/TodoActions'
+import Input from '../ui/Input'
 
-const DeleteTodo = ({ todo }: { todo: todoTypes }) => {
-  const router = useRouter()
-  const [isPending, startTransition] = useTransition()
-
-  // Wrap the deleteTodo action to refresh page after success
-  const handleDelete = async (formData: FormData) => {
-    await deleteTodo(formData)
-    startTransition(() => {
-      router.refresh()  // refreshes server components, updating UI
-    })
-  }
-
+const DeleteTodo = ({todo}:{todo:todoTypes}) => {
   return (
     <div>
-      <Form action={handleDelete}>
-        <Input type="hidden" value={todo.id} name="inputId" />
-        <Button
-          actionButton
-          type="submit"
-          text={
-            <>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 bg-red-800 rounded-full"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-              </svg>
-            </>
-          }
+        
+        {/* Delete Todo {todo.id} */}
+        <div>
+            <Form action={deleteTodo}>
+            
+            <Input
+                type='hidden'
+                value={todo.id}
+                name="inputId"
+            />
+
+
+<Button
+  actionButton
+  type="submit"
+  text={
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6 bg-red-800 rounded-full"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
         />
-      </Form>
+      </svg>
+    </>
+  }
+/>
+
+
+            </Form>
+        </div>
+
+
     </div>
   )
 }
 
 export default DeleteTodo
+
+
+
+// "use client"
+// import React from "react"
+// import { todoTypes } from "../../../types/todoTypes"
+// import Form from "../ui/Form"
+// import Button from "../ui/Button"
+// import { deleteTodo } from "@/app/actions/TodoActions"
+// import Input from "../ui/Input"
+// import { useRouter } from "next/navigation"
+// import { useTransition } from "react"
+
+// const DeleteTodo = ({ todo }: { todo: todoTypes }) => {
+//   const router = useRouter()
+//   const [isPending, startTransition] = useTransition()
+
+//   // Wrap the deleteTodo action to refresh page after success
+//   const handleDelete = async (formData: FormData) => {
+//     await deleteTodo(formData)
+//     startTransition(() => {
+//       router.refresh()  // refreshes server components, updating UI
+//     })
+//   }
+
+//   return (
+//     <div>
+//       <Form action={handleDelete}>
+//         <Input type="hidden" value={todo.id} name="inputId" />
+//         <Button
+//           actionButton
+//           type="submit"
+//           text={
+//             <>
+//               <svg
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 fill="none"
+//                 viewBox="0 0 24 24"
+//                 strokeWidth={1.5}
+//                 stroke="currentColor"
+//                 className="w-6 h-6 bg-red-800 rounded-full"
+//               >
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+//                 />
+//               </svg>
+//             </>
+//           }
+//         />
+//       </Form>
+//     </div>
+//   )
+// }
+
+// export default DeleteTodo
